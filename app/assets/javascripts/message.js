5e1013f8d7c1a03de81fc173fa83a2b08df49ed6
tamataku <takuya.kokura0815@icloud.com>
2020-05-01 20:03:15 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,39 +2,32 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
+       `<div class="message" data-message-id=${message.id}>
+          <div class="message-box__user-name">
+            ${message.user_name}
+            <div class="message-box__date-time">
               ${message.created_at}
             </div>
           </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
+        </div>
+          <p class="message-box__user-message">
+            ${message.content}
+          </p>
+        <img src=${message.image} >
+      </div>`
       return html;
     } else {
       var html =
-       `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
+       `<div class="message" data-message-id=${message.id}>
+          <div class="message-box__user-name">
+            ${message.user_name}
+            <div class="message-box__date-time">
               ${message.created_at}
             </div>
           </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
+          <p class="message-box__user-message">
+            ${message.content}
+          </p>
         </div>`
       return html;
     };
@@ -62,4 +55,31 @@ $(function(){
     });
   return false;
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
